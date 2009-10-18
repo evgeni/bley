c2b87138db6cecdfb82d3524d6ce05c5293f67ca
Evgeni Golov <evgeni.golov@uni-duesseldorf.de>
2009-10-18 17:21:14 +0000

    port BleyWorker to PostfixPolicy interface
    git-svn-id: https://svncn.cs.uni-duesseldorf.de/cn/trunk/people/students/golov/bley@31163 bfee2fec-0ff8-0310-9bd0-901819df4ca6

--- BleyWorker.py
+++ BleyWorker.py
@@ -32,95 +32,82 @@ import psycopg2
 import adns
 import datetime
 from BleyHelpers import *
+from PostfixPolicy import PostfixPolicy
 
-class BleyWorker (Thread):
+class BleyWorker (PostfixPolicy, Thread):
 	
-	csocket = None
 	settings = None
-	postfix_params = {}
 	db = None
 	dbc = None
 	adns_handle = None
 	
 	def __init__ (self, csocket, settings):
-		self.csocket = csocket
 		self.settings = settings
-		self.postfix_params = {}
 		self.db = self.settings.database.connect(self.settings.dsn)
 		self.dbc = self.db.cursor()
 		self.adns_handle = adns.init()
+		PostfixPolicy.__init__(self, csocket)
 		Thread.__init__(self)
 	
 	def run(self):
-		while True:
-			check_results = {'DNSWL': 0, 'DNSBL': 0, 'HELO': 0, 'DYN': 0, 'DB': -1, 'SPF': 0, 'S_EQ_R': 0 }
-			buff = self.csocket.recv(4096)
-			if not buff: break
-			lines = buff.splitlines()
-			for line in lines:
-				line = line.strip()
-				if line == '':
-					break
-				else:
-					try:
-						(pkey, pval) = line.split('=')
-						self.postfix_params[pkey] = pval
-					except:
-						print 'Could not parse "%s"' % line
+		self.parse_input()
 
+	def check_policy (self, postfix_params):
+		check_results = {'DNSWL': 0, 'DNSBL': 0, 'HELO': 0, 'DYN': 0, 'DB': -1, 'SPF': 0, 'S_EQ_R': 0 }
+		action = 'DUNNO'
+
+		status = self.check_local_db(postfix_params)
+		# -1 : not found
+		#  0 : regular host, not in black, not in white, let it go
+		#  1 : regular host, but in white, let it go, dont check EHLO
+		#  2 : regular host, but in black, lets grey for now
+		if postfix_params['recipient'].lower().startswith('postmaster'):
 			action = 'DUNNO'
-			status = self.check_local_db(self.postfix_params)
-			# -1 : not found
-			#  0 : regular host, not in black, not in white, let it go
-			#  1 : regular host, but in white, let it go, dont check EHLO
-			#  2 : regular host, but in black, lets grey for now
-			if self.postfix_params['recipient'].lower().startswith('postmaster'):
-				action = 'DUNNO'
-			elif status == -1: # not found in local db...
-				check_results['DNSWL'] = self.check_dnswls(self.postfix_params['client_address'])
-				if check_results['DNSWL'] >= self.settings.dnswl_threshold:
-					new_status = 1
-				else:
-					check_results['DNSBL'] = self.check_dnsbls(self.postfix_params['client_address'])
-					check_results['HELO'] = check_helo(self.postfix_params)
-					check_results['DYN'] = is_dyn_host(self.postfix_params['client_name'])
-					check_results['SPF'] = check_spf(self.postfix_params)
-					if self.postfix_params['sender']==self.postfix_params['recipient']:
-						check_results['S_EQ_R'] = 1
-					if check_results['DNSBL'] >= self.settings.dnsbl_threshold or check_results['HELO']+int(check_results['DYN'])+check_results['SPF']+check_results['S_EQ_R'] >= self.settings.rfc_threshold:
-						new_status = 2
-						action = 'DEFER_IF_PERMIT %s' % self.settings.reject_msg
-					else:
-						new_status = 0
-	                        query = "INSERT INTO bley_status (ip, status, last_action, last_from, last_to) VALUES(%(client_address)s, %(new_status)s, 'now', %(sender)s, %(recipient)s)"
-				params = self.postfix_params.copy()
-				params['new_status'] = new_status
-	                        self.dbc.execute(query, params)
-	                        self.db.commit()
-
-			elif status[0] >= 2: # found to be greyed
-				check_results['DB'] = status[0]
-				delta = datetime.datetime.now()-status[1]
-				if delta > self.settings.greylist_period+status[2]*self.settings.greylist_penalty or delta > self.settings.greylist_max:
-					action = 'DUNNO'
-					query = "UPDATE bley_status SET status=0, last_action='now', last_from=%(sender)s, last_to=%(recipient)s WHERE ip=%(client_address)s"
-					self.dbc.execute(query, self.postfix_params)
-					self.db.commit()
-				else:
+		elif status == -1: # not found in local db...
+			check_results['DNSWL'] = self.check_dnswls(postfix_params['client_address'])
+			if check_results['DNSWL'] >= self.settings.dnswl_threshold:
+				new_status = 1
+			else:
+				check_results['DNSBL'] = self.check_dnsbls(postfix_params['client_address'])
+				check_results['HELO'] = check_helo(postfix_params)
+				check_results['DYN'] = is_dyn_host(postfix_params['client_name'])
+				check_results['SPF'] = check_spf(postfix_params)
+				if postfix_params['sender']==postfix_params['recipient']:
+					check_results['S_EQ_R'] = 1
+				if check_results['DNSBL'] >= self.settings.dnsbl_threshold or check_results['HELO']+int(check_results['DYN'])+check_results['SPF']+check_results['S_EQ_R'] >= self.settings.rfc_threshold:
+					new_status = 2
 					action = 'DEFER_IF_PERMIT %s' % self.settings.reject_msg
-					query = "UPDATE bley_status SET fail_count=fail_count+1, last_action='now', last_from=%(sender)s, last_to=%(recipient)s WHERE ip=%(client_address)s"
-					self.dbc.execute(query, self.postfix_params)
-					self.db.commit()
+				else:
+					new_status = 0
+                        query = "INSERT INTO bley_status (ip, status, last_action, last_from, last_to) VALUES(%(client_address)s, %(new_status)s, 'now', %(sender)s, %(recipient)s)"
+			params = postfix_params.copy()
+			params['new_status'] = new_status
+                        self.dbc.execute(query, params)
+                        self.db.commit()
 
-			else: # found to be clean
-				check_results['DB'] = status[0]
+		elif status[0] >= 2: # found to be greyed
+			check_results['DB'] = status[0]
+			delta = datetime.datetime.now()-status[1]
+			if delta > self.settings.greylist_period+status[2]*self.settings.greylist_penalty or delta > self.settings.greylist_max:
 				action = 'DUNNO'
-				query = "UPDATE bley_status SET last_action='now', last_from=%(sender)s, last_to=%(recipient)s WHERE ip=%(client_address)s"
-				self.dbc.execute(query, self.postfix_params)
+				query = "UPDATE bley_status SET status=0, last_action='now', last_from=%(sender)s, last_to=%(recipient)s WHERE ip=%(client_address)s"
+				self.dbc.execute(query, postfix_params)
+				self.db.commit()
+			else:
+				action = 'DEFER_IF_PERMIT %s' % self.settings.reject_msg
+				query = "UPDATE bley_status SET fail_count=fail_count+1, last_action='now', last_from=%(sender)s, last_to=%(recipient)s WHERE ip=%(client_address)s"
+				self.dbc.execute(query, postfix_params)
 				self.db.commit()
 
-			self.settings.logger('decided action=%s, checks: %s, postfix: %s\n' % (action, check_results, self.postfix_params))
-			self.csocket.sendall('action=%s\n\n' % action)
+		else: # found to be clean
+			check_results['DB'] = status[0]
+			action = 'DUNNO'
+			query = "UPDATE bley_status SET last_action='now', last_from=%(sender)s, last_to=%(recipient)s WHERE ip=%(client_address)s"
+			self.dbc.execute(query, postfix_params)
+			self.db.commit()
+
+		self.settings.logger('decided action=%s, checks: %s, postfix: %s\n' % (action, check_results, postfix_params))
+		self.send_action(action)
 
 	def check_local_db(self, postfix_params):
 		query = "SELECT status,last_action,fail_count FROM bley_status WHERE ip=%(client_address)s LIMIT 1"
