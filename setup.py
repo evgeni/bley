7550327888306f870b83a5f020668fa0fd25ab9c
Evgeni Golov <sargentd@die-welt.net>
2010-02-25 23:40:26 +0100

    add setup.py using setuptools

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "bley",
+    version = "0.1",
+    description = "intelligent greylisting daemon for postfix",
+    author = "Evgeni Golov",
+    author_email = "sargentd@die-welt.net",
+    url = "http://bley.mx",
+    license = "BSD",
+    py_modules = ['bley', 'bleyhelpers', 'postfix'],
+    scripts = ['bley'],
+    zip_safe = False,
+    # install_requires = [ needs to be filled! ],
+)
