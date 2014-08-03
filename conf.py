0c1cd9f97c2124022fb6224e4e64b86c3254ce30
Evgeni Golov <evgeni@golov.de>
2014-08-03 10:31:36 +0200

    add rsync deploy

--- conf.py
+++ conf.py
@@ -288,7 +288,7 @@ REDIRECTIONS = []
 # And then do a backup, or run `nikola ping` from the `ping`
 # plugin (`nikola install_plugin ping`).
 # To do manual deployment, set it to []
-# DEPLOY_COMMANDS = []
+DEPLOY_COMMANDS = ['rsync -rav --delete output/ root@shokki.die-welt.net:/srv/syscp/webs/sargentd/bley.mx/']
 
 # For user.github.io/organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
